054999e0f774816a1b1c6a97dac905fca251c2db
XenaaLangliss <f4d1lcyber@gmail.com>
2025-12-25 02:27:50 +0700

    Update

--- service.sh
+++ service.sh
@@ -1,15 +1,19 @@
-#!/system/bin/sh
-
-# ==============================
-# BOOT
-# ==============================
-while [ "$(getprop sys.boot_completed)" != "1" ]; do
+#!/bin/sh
+wait_until_login() {
+  until [ "$(getprop sys.boot_completed)" -eq 1 ]; do
     sleep 1
-done
+  done
 
-# ==============================
-# MAIN
-# ==============================
-ROZEN="/system/bin/rozencharge"
-[ -x "$ROZEN" ] || exit 0
-"$ROZEN" >/dev/null 2>&1 &
+  #test file
+  test_file="/storage/emulated/0/.test"
+  until touch "$test_file" 2>/dev/null; do
+    sleep 1
+  done
+  rm -f "$test_file"
+}
+
+wait_until_login
+sleep 15
+
+# Main
+rozencharge
